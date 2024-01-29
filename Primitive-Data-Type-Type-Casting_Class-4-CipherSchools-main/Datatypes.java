32f41b97d59f8d6ac9c46182e79d785ae47df7aa
ramkoteswarao <117813050+ramkoteswarao@users.noreply.github.com>
2024-01-29 22:05:59 +0530

    Add files via upload

---
+++ Primitive-Data-Type-Type-Casting_Class-4-CipherSchools-main/Datatypes.java
@@ -0,0 +1,12 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int x=500;
+        int y=7;
+        byte m=(byte)x;
+        System.out.println("x is:"+x+m);
+        System.out.println("y is:"+y);
+        byte b=(byte)1000;
+        int k=(int)x+b;
+        System.out.println("b is:"+b+k);
+    }
+}
